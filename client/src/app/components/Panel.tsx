2015e14b591007adb44a4d33ac7b8ae942a92d61
patkamon <patkamon.a@ku.th>
2024-04-15 22:08:41 +0700

    add panel

---
+++ client/src/app/components/Panel.tsx
@@ -0,0 +1,14 @@
+'use client'
+
+
+export default function Panel() {
+
+
+  return (
+      <div className="block w-[20rem] h-[20rem] relative bg-red-300">
+
+        hello2
+      </div>
+  )
+}
+
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,9 +1,17 @@
+import Panel from "./components/Panel";
 import Sidebar from "./components/Sidebar";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex w-screen">
    <Sidebar/> 
+   <div className="flex justify-evenly bg-blue-300 w-full">
+   <Panel/>
+   <Panel/>
+   <Panel/>
+   </div>
+
+
     </div>
   );
 }
