d54d652cfe0d28ce5604429080d447eba0361895
patkamon <patkamon.a@ku.th>
2024-04-16 17:38:05 +0700

    Add test

--- backend/src/concert/controllers/concert/concert.controller.spec.ts
+++ backend/src/concert/controllers/concert/concert.controller.spec.ts
@@ -1,47 +1,45 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ConcertController } from './concert.controller';
-import { getRepositoryToken } from '@nestjs/typeorm';
-import { Concert } from 'src/typeorm/entities/Concert';
-import { Reservation } from 'src/typeorm/entities/Reservation';
 import { ConcertService } from 'src/concert/services/concert/concert.service';
 
 describe('ConcertController', () => {
   let controller: ConcertController;
 
+  const mockConcertService = {
+    createConcert: jest.fn((header, dto) => {
+      if (header.role == 'Admin') {
+        console.log(dto);
+        return undefined;
+      }
+    }),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ConcertController],
-      providers: [
-        ConcertService,
-        {
-          provide: getRepositoryToken(Concert),
-          useValue: {
-            find: jest.fn(),
-            findOne: jest.fn(),
-            findOneBy: jest.fn(),
-            create: jest.fn(),
-            save: jest.fn(),
-            remove: jest.fn(),
-          },
-        },
-        {
-          provide: getRepositoryToken(Reservation),
-          useValue: {
-            find: jest.fn(),
-            findOne: jest.fn(),
-            create: jest.fn(),
-            save: jest.fn(),
-            remove: jest.fn(),
-            query: jest.fn(),
-          },
-        },
-      ],
-    }).compile();
+      providers: [ConcertService],
+    })
+      .overrideProvider(ConcertService)
+      .useValue(mockConcertService)
+      .compile();
 
     controller = module.get<ConcertController>(ConcertController);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('should create concert', () => {
+    expect(
+      controller.createConcert(
+        { role: 'Admin' },
+        {
+          name: 'test',
+          description: 'desc',
+          seat: 10,
+        },
+      ),
+    ).toEqual(undefined);
   });
 });
